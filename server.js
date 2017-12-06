8698848db410fcd0372e8c422049c7c97a8625d1
indrashekargr <indrashekargr@gmail.com>
2017-12-06 11:45:38 +0530

    first push
    first push

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var port = process.env.PORT || 8000; 
+
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var swaggerize = require('swaggerize-express');
+var swaggerUi = require('swaggerize-ui'); 
+var path = require('path');
+var fs = require("fs");
+
+fs.existsSync = fs.existsSync || require('path').existsSync;
+
+var app = express();
+
+var server = http.createServer(app);
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+res.sendFile(__dirname + '/dist/index.html');
+    
+});
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/app')));
+app.use(swaggerize({
+    api: path.resolve('./config/swagger.json'),
+    handlers: path.resolve('./handlers'),
+    docspath: '/swagger' 
+}));
+
+// change four
+app.use('/docs', swaggerUi({
+    docs: '/swagger'  
+}));
+
+server.listen(port, function () { 
+});
